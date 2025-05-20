d5582841a0361458a83e50f160c6e3c9a5f9c7ee
Fabricio Jerez <fabrijerez1@gmail.com>
2025-05-19 22:17:50 -0300

    add test for RemoveAccentMarks() tests

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -18,14 +18,36 @@ func TestGetCapital (t *testing.T) {
 	
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			capital, err := GetCapital(tt.province)
+			receivedCapital, err := GetCapital(tt.province)
 			
 			if err != nil {
-				t.Errorf("GetCapital(%s) = %s, %v; want %s, nil", tt.province, capital, err, tt.expectedCapital)
+				t.Errorf("GetCapital(%s) = %s, %v; want %s, nil", tt.province, receivedCapital, err, tt.expectedCapital)
 			}
-			if capital != tt.expectedCapital {
-				t.Errorf("GetCapital(%s) = %s; want %s", tt.province, capital, tt.expectedCapital)
+			if receivedCapital != tt.expectedCapital {
+				t.Errorf("GetCapital(%s) = %s; want %s", tt.province, receivedCapital, tt.expectedCapital)
+			}
+ 		})
+	}
+}
+
+func TestRemoveAccentMarks (t *testing.T) {
+	tests := []struct {
+		name     string
+		entryString     string
+		expectedString string
+	}{
+		{"string with accent marks", "ríáchúéló", "riachuelo"},
+		{"string without accent marks", "Totalidad", "Totalidad"},
+		{"string with other marks", "pingüino", "pingüino"},
+	}
+	
+	for _,tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			receivedString := RemoveAccentMarks(tt.entryString)
+			
+			if receivedString != tt.entryString {
+				t.Errorf("RemoveActionMarks(%s) = %s; want %s", tt.entryString, receivedString, tt.expectedString)
 			}
 		})
-	}
+		}
 }

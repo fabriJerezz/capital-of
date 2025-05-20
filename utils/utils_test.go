b04e7298d7b204881b64058330b5a04655000621
Fabricio Jerez <fabrijerez1@gmail.com>
2025-05-19 21:49:06 -0300

    change the test files to follow go convention

---
+++ utils/utils_test.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"testing"
+)
+
+
+func TestGetCapital (t *testing.T) {
+	tests := []struct {
+		name     string
+		province     string
+		expectedCapital string
+	}{
+		{"real province", "Chaco", "Resistencia"},
+		{"case and accent marks", "sÁnTá fÉ", "Santa Fé"},
+		{"another word", "Chiavenato", "No se encontró la provincia Chiavenat"},
+	}
+	
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			capital, err := GetCapital(tt.province)
+			
+			if err != nil {
+				t.Errorf("GetCapital(%s) = %s, %v; want %s, nil", tt.province, capital, err, tt.expectedCapital)
+			}
+			if capital != tt.expectedCapital {
+				t.Errorf("GetCapital(%s) = %s; want %s", tt.province, capital, tt.expectedCapital)
+			}
+			
+		})
+	}
+}
